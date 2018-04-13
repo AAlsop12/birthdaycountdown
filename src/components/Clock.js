97d6a45308eeb441b8a9be80f4f5c9f84a41022f
AAlsop12 <aimee.johansen33@gmail.com>
2018-04-12 18:42:25 -0600

    Moved mixin moved countdown tag and finished clock styles

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -100,17 +100,19 @@ class Clock extends Component {
     render() {
         const data = this.state.timeRemaining
         return (
-             <div className="countdown">
+  <div>
              {
                  this.state.timeRemaining == 0 ?
-                 <div className="message-container">
-                     <p className="message-container__title">Countdown Complete</p>
-                     <p className="message-container__message">HAPPY BIRTHDAY!</p>
+                 <div className="countdown">
+                    <div className="message-container">
+                        <p className="message-container__title">Countdown Complete</p>
+                        <p className="message-container__message">HAPPY BIRTHDAY!</p>
+                    </div>
                  </div>
+                 
                     :
                     <div>
-                        <div>
-                            <ul className="countdown__clock">
+                        <div className="countdown">                            <ul className="countdown__clock">
                                 <li>DAYS<p>{data.days}</p></li>
                                 <li>HRS<p>{data.hours}</p></li>
                                 <li>MINS<p>{data.minutes}</p></li>
@@ -118,7 +120,7 @@ class Clock extends Component {
                             </ul>
                             
                         </div>
-                        <div>
+                        <div className="until-container">
                             {this.renderMessage()}
                         </div>
                     </div>
